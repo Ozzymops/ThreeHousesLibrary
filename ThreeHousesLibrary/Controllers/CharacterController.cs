42fda0492f9a3bd6a16116f7f829373951a1ebbb
Ozzymops <justinmuris@gmail.com>
2020-01-29 19:12:08 +0100

    Byleth, Jeralt

--- ThreeHousesLibrary/Controllers/CharacterController.cs
+++ ThreeHousesLibrary/Controllers/CharacterController.cs
@@ -8,9 +8,25 @@ namespace ThreeHousesLibrary.Controllers
 {
     public class CharacterController : Controller
     {
+        // Unaffiliated
         public IActionResult Byleth()
         {
             return View();
         }
+
+        public IActionResult Jeralt()
+        {
+            return View();
+        }
+
+        public IActionResult Sothis()
+        {
+            return View();
+        }
+
+        // Black Eagles
+        // Blue Lions
+        // Golden Deer
+        // Church of Seiros
     }
 }
