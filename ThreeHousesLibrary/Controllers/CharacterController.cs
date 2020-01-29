e18f7a397ae76608b743fbaf48f8c2b079134a18
Ozzymops <justinmuris@gmail.com>
2020-01-29 18:08:15 +0100

    Add project files.

---
+++ ThreeHousesLibrary/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreeHousesLibrary.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult Byleth()
+        {
+            return View();
+        }
+    }
+}
