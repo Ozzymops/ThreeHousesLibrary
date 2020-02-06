0b6bf9359fbdb02a78ca8669f3aa7ad94dbe60aa
Ozzymops <justinmuris@gmail.com>
2020-02-06 14:07:47 +0100

    Added Class Views, populated Class Controller, added Commoner, Noble, Enlightened One

--- ThreeHousesLibrary/Controllers/ClassController.cs
+++ ThreeHousesLibrary/Controllers/ClassController.cs
@@ -8,6 +8,7 @@ namespace ThreeHousesLibrary.Controllers
 {
     public class ClassController : Controller
     {
+        // Unique
         public IActionResult Commoner()
         {
             return View();
@@ -17,5 +18,236 @@ namespace ThreeHousesLibrary.Controllers
         {
             return View();
         }
+
+        public IActionResult Dancer()
+        {
+            return View();
+        }
+
+        public IActionResult EnlightenedOne()
+        {
+            return View();
+        }
+
+        public IActionResult ArmoredLord()
+        {
+            return View();
+        }
+
+        public IActionResult Emperor()
+        {
+            return View();
+        }
+
+        public IActionResult HighLord()
+        {
+            return View();
+        }
+
+        public IActionResult GreatLord()
+        {
+            return View();
+        }
+
+        public IActionResult WyvernMaster()
+        {
+            return View();
+        }
+
+        public IActionResult Barbarossa()
+        {
+            return View();
+        }
+
+        // Beginner
+        public IActionResult Myrmidon()
+        {
+            return View();
+        }
+
+        public IActionResult Soldier()
+        {
+            return View();
+        }
+
+        public IActionResult Fighter()
+        {
+            return View();
+        }
+
+        public IActionResult Monk()
+        {
+            return View();
+        }
+
+
+        // Intermediate
+        public IActionResult Lord()
+        {
+            return View();
+        }
+
+        public IActionResult Mercenary()
+        {
+            return View();
+        }
+
+        public IActionResult Thief()
+        {
+            return View();
+        }
+
+        public IActionResult ArmoredKnight()
+        {
+            return View();
+        }
+
+        public IActionResult Cavalier()
+        {
+            return View();
+        }
+
+        public IActionResult Brigand()
+        {
+            return View();
+        }
+
+        public IActionResult Archer()
+        {
+            return View();
+        }
+
+        public IActionResult Brawler()
+        {
+            return View();
+        }
+
+        public IActionResult Mage()
+        {
+            return View();
+        }
+
+        public IActionResult DarkMage()
+        {
+            return View();
+        }
+
+        public IActionResult Priest()
+        {
+            return View();
+        }
+
+        public IActionResult PegasusKnight()
+        {
+            return View();
+        }
+
+        // Advanced
+        public IActionResult Hero()
+        {
+            return View();
+        }
+
+        public IActionResult Swordmaster()
+        {
+            return View();
+        }
+
+        public IActionResult Assassin()
+        {
+            return View();
+        }
+
+        public IActionResult FortressKnight()
+        {
+            return View();
+        }
+
+        public IActionResult Paladin()
+        {
+            return View();
+        }
+
+        public IActionResult WyvernRider()
+        {
+            return View();
+        }
+
+        public IActionResult Warrior()
+        {
+            return View();
+        }
+
+        public IActionResult Sniper()
+        {
+            return View();
+        }
+
+        public IActionResult Grappler()
+        {
+            return View();
+        }
+
+        public IActionResult Warlock()
+        {
+            return View();
+        }
+
+        public IActionResult DarkBishop()
+        {
+            return View();
+        }
+
+        public IActionResult Bishop()
+        {
+            return View();
+        }
+
+        // Master
+        public IActionResult FalconKnight()
+        {
+            return View();
+        }
+
+        public IActionResult WyvernLord()
+        {
+            return View();
+        }
+
+        public IActionResult MortalSavant()
+        {
+            return View();
+        }
+
+        public IActionResult GreatKnight()
+        {
+            return View();
+        }
+
+        public IActionResult BowKnight()
+        {
+            return View();
+        }
+
+        public IActionResult DarkKnight()
+        {
+            return View();
+        }
+
+        public IActionResult HolyKnight()
+        {
+            return View();
+        }
+
+        public IActionResult WarMaster()
+        {
+            return View();
+        }
+
+        public IActionResult Gremory()
+        {
+            return View();
+        }
+
     }
 }
