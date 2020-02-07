d6582f646042512c33cf5d68f70e1a0698302365
Ozzymops <justinmuris@gmail.com>
2020-02-07 19:18:03 +0100

    Edited JS scripts for Characters & Class sidebars, added all Unique and Beginner classes.

--- ThreeHousesLibrary/wwwroot/js/classSidebar.js
+++ ThreeHousesLibrary/wwwroot/js/classSidebar.js
@@ -1,24 +1,24 @@
 ﻿// Unique
-if (document.title.includes("Commoner") || document.title.includes("Noble") || document.title.includes("Dancer") || document.title.includes("Enlightened One") || document.title.includes("Armored Lord") || document.title.includes("Emperor") || document.title.includes("High Lord") || document.title.includes("Great Lord") || document.title.includes("Wyvern Master") || document.title.includes("Barbarossa")) {
+if (document.getElementById('group').value == 0) {
     $("#unique").addClass("show");
 }
 
 // Beginner
-if (document.title.includes("Myrmidon") || document.title.includes("Soldier") || document.title.includes("Fighter") || document.title.includes("Monk")) {
+if (document.getElementById('group').value == 1) {
     $("#beginner").addClass("show");
 }
 
 // Intermediate
-if (document.title.includes("Lord") || document.title.includes("Mercenary") || document.title.includes("Thief") || document.title.includes("Armored Knight") || document.title.includes("Cavalier") || document.title.includes("Brigand") || document.title.includes("Archer") || document.title.includes("Brawler") || document.title.includes("Mage") || document.title.includes("Dark Mage") || document.title.includes("Priest") || document.title.includes("Pegasus Knight")) {
+if (document.getElementById('group').value == 2) {
     $("#intermediate").addClass("show");
 }
 
 // Advanced
-if (document.title.includes("Hero") || document.title.includes("Swordmaster") || document.title.includes("Assassin") || document.title.includes("Fortress Knight") || document.title.includes("Paladin") || document.title.includes("Wyvern Rider") || document.title.includes("Warrior") || document.title.includes("Sniper") || document.title.includes("Grappler") || document.title.includes("Warlock") || document.title.includes("Dark Bishop") || document.title.includes("Bishop")) {
+if (document.getElementById('group').value == 3) {
     $("#advanced").addClass("show");
 }
 
 // Master
-if (document.title.includes("Falcon Knight") || document.title.includes("Wyvern Lord") || document.title.includes("Mortal Savant") || document.title.includes("Great Knight") || document.title.includes("Bow Knight") || document.title.includes("Dark Knight") || document.title.includes("Holy Knight") || document.title.includes("War Master") || document.title.includes("Gremory")) {
+if (document.getElementById('group').value == 4) {
     $("#master").addClass("show");
 }
